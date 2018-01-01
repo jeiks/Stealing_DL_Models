c453f8963e6a735cb4c4c64452a00e663f76577d
Jacson Rodrigues Correia da Silva <jacsonrcsilva@gmail.com>
2018-01-01 00:02:31 -0200

    Without training - VGG - CIFAR

---
+++ Models/cifar/vgg_without_training/create_weights.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2
+
+import caffe
+caffe.set_mode_gpu()
+caffe.set_device(1)
+net = caffe.Net('vgg_cifar.prototxt', 'VGG_ILSVRC_16_layers.caffemodel', caffe.TRAIN)
+net.save('weights.caffemodel')
