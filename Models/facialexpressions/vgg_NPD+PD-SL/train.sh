fd88883339ee5289cfd5ffa4d501c99f2156f5b1
Jacson Rodrigues Correia da Silva <jacsonrcsilva@gmail.com>
2018-02-03 16:56:20 -0200

    Facial Expressions - VGG - NDP+DP-SL

---
+++ Models/facialexpressions/vgg_NPD+PD-SL/train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#CAFFE:
+CAFFE_BIN='/home/jeiks/Doutorado/caffe/build/tools/caffe'
+
+#PROTOTXT FILES:
+PROTOTXT_PATH="$PWD/prototxt"
+SOLVER="$PROTOTXT_PATH/solver.prototxt"
+WEIGHTS='snapshot_iter_534470.caffemodel'
+GPU=1
+LOG_FILE=$(date "+log-%Y_%m_%d-%H_%M.txt")
+
+echo "Using:
+PROTOTXT_PATH = $PROTOTXT_PATH
+SOLVER        = $SOLVER
+WEIGHTS       = $WEIGHTS
+GPU           = $GPU
+"
+read -n1 -p 'Press ENTER to start training... '
+
+$CAFFE_BIN train --solver=$SOLVER --weights=$WEIGHTS -gpu=$GPU 2>&1 | tee $LOG_FILE
